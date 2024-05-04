6c107df1e25a4ce87bb3936947f26427d1386de3
shlyyy <shlyyy@foxmail.com>
2024-05-04 23:22:57 +0800

    这是vs提交测试

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 
 int main(int argc, char* argv[])
 {
+    std::cout << "begin main" << std::endl;
+
     std::cout << "hello stl" << std::endl;
+
     std::cout << "end main" << std::endl;
     return 0;
 }
